c49377c6e089049e4f69aedb8da89179cac79cdc
jj09 <jakub.jedryszek@gmail.com>
2016-01-02 00:03:09 -0800

    iOS prototyped (exception throwing on startup)

--- BreadCrumbs.iOS/ViewController.cs
+++ BreadCrumbs.iOS/ViewController.cs
@@ -1,25 +1,83 @@
 ﻿using System;
 
 using UIKit;
+using BreadCrumbs.Shared.ViewModels;
+using System.Linq;
+using Foundation;
+using BreadCrumbs.Shared.Models;
 
 namespace BreadCrumbs.iOS
 {
 	public partial class ViewController : UIViewController
 	{
+		MainViewModel ViewModel;
+
 		public ViewController (IntPtr handle) : base (handle)
 		{
+			ViewModel = new MainViewModel();
+
+			this.saveButton.TouchUpInside += (object sender, EventArgs e) => 
+			{
+				var name = this.placeNameTextField.Text;
+				ViewModel.SaveAsync(name, new Coordinates(1,2));
+			};
 		}
 
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
 			// Perform any additional setup after loading the view, typically from a nib.
+			PlacesTableView.Source = new PlacesTableSource(ViewModel);
 		}
 
 		public override void DidReceiveMemoryWarning ()
 		{
 			base.DidReceiveMemoryWarning ();
 			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+//			TableView.ReloadData();
+		}
+	}
+
+	public class PlacesTableSource : UITableViewSource
+	{
+		string CellIdentifier = "TableCell";
+
+		MainViewModel ViewModel;
+
+
+		public PlacesTableSource (MainViewModel vm)
+		{
+			ViewModel = vm;
+		}
+
+		public override nint RowsInSection (UITableView tableview, nint section)
+		{
+			return ViewModel.SavedPlaces.Count;
+		}
+
+		public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
+		{
+			UITableViewCell cell = tableView.DequeueReusableCell (CellIdentifier);
+			var item = ViewModel.SavedPlaces.ElementAt(indexPath.Row);
+
+			if (cell == null)
+			{
+				cell = new UITableViewCell (UITableViewCellStyle.Default, CellIdentifier);
+			}
+
+			cell.TextLabel.Text = item.Name;
+
+			return cell;
+		}
+
+		public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
+		{
+			// TODO: navigate to place
 		}
 	}
 }
--- BreadCrumbs.iOS/ViewController.designer.cs
+++ BreadCrumbs.iOS/ViewController.designer.cs
@@ -1,17 +1,45 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace BreadCrumbs.iOS
 {
 	[Register ("ViewController")]
 	partial class ViewController
 	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField placeNameTextField { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITableView PlacesTableView { get; set; }
+
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton saveButton { get; set; }
+
 		void ReleaseDesignerOutlets ()
 		{
+			if (placeNameTextField != null) {
+				placeNameTextField.Dispose ();
+				placeNameTextField = null;
+			}
+			if (PlacesTableView != null) {
+				PlacesTableView.Dispose ();
+				PlacesTableView = null;
+			}
+			if (saveButton != null) {
+				saveButton.Dispose ();
+				saveButton = null;
+			}
 		}
 	}
 }
